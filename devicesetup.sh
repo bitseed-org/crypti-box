9eb842af90545fa01c9faad2deefe8398a170f15
jfeldis <jfeldis@gmail.com>
2015-01-18 21:29:06 -0800

    Create devicesetup.sh

---
+++ devicesetup.sh
@@ -0,0 +1,20 @@
+sudo apt-get install unzip
+sudo apt-get install -y nodejs
+sudo apt-get install -y nodejs-legacy
+sudo apt-get install -y nodejs
+sudo apt-get install -y sqlite3
+node -v
+sudo apt-get update
+sudo apt-get install npm
+npm -v
+sudo apt-get install build-essential
+wget http://storage.googleapis.com/downloads.crypti.me/crypti-node/testing/0.1.9.zip
+unzip 0.1.9.zip
+npm install
+cd node_modules
+cd ed25519
+node-gyp configure
+node-gyp build
+cd /home/crypti/0.1.9
+npm install
+sudo npm install -g forever
